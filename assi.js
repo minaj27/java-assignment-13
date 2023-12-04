5bee036e8ffba729ecf688c8d52638ce0c194429
minaj27 <minaj13m@gmail.com>
2023-12-05 00:33:55 +0530

    first commit

---
+++ assi.js
@@ -0,0 +1,12 @@
+function handleHeight(H){
+    document.getElementById("show").style.height=H;
+}
+function handleWidth(W){
+    document.getElementById("show").style.width=W;
+}
+function handleBorder(border){
+    document.getElementById("show").style.borderRadius=border;
+}
+function handleButton(clr){
+    document.getElementById("show").style.backgroundColor=clr;
+}
